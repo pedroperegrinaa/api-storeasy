0f0093967775a0bb985b09fe3dfa0448731aebfc
pedroperegrinaa <57403682+pedroperegrinaa@users.noreply.github.com>
2022-05-27 22:55:55 -0300

    refactor: CRUD CartsController

--- README.md
+++ README.md
@@ -35,3 +35,19 @@ Response: return all carts
 </code></pre>
 
 create a cart on database
+
+### UPDATE
+
+    PUT /carts
+
+<pre><code>{
+	"code": "1234",
+	"price": 22,
+	"_id": "62914da8b91a7dc1f64e31b0",
+	"createdAt": "2022-05-27T22:16:08.812Z",
+	"updatedAt": "2022-05-27T22:16:08.812Z",
+	"__v": 0
+}
+</code></pre>
+
+create a cart on database
--- src/controllers/CartsController.js
+++ src/controllers/CartsController.js
@@ -25,23 +25,22 @@ class CartsController {
 
   async update(req, res) {
     console.log("update");
+    const { id } = req.params;
+    const { code, price } = req.body;
+
+    if (!id) {
+      console.log(id);
+      return res.status(400).json({ error: "Missing id." });
+    }
+
     try {
-      const { code, price } = req.body;
+      const cart = await Cart.findById(id);
 
-      const { id } = req.params;
-
-      if (!id) {
-        console.log(id);
-        return res.status(400).json({ error: "Missing id." });
-      }
-
-      try {
-        const cart = await Cart.findById(req.params.id);
-      } catch (error) {
+      if (!cart) {
         return res.status(400).json({ error: "Carrinho não existe." });
+      } else {
+        cart.updateOne({ code, price });
       }
-
-      await cart.updateOne({ code, price });
 
       return res.status(200).json(cart);
     } catch (error) {
@@ -51,18 +50,23 @@ class CartsController {
 
   async delete(req, res) {
     console.log("delete");
-    try {
-      const { id } = req.params;
 
-      if (!id) {
-        console.log(id);
-        return res.status(400).json({ error: "Missing id." });
-      }
-      try {
-        const cart = await Cart.findById(req.params.id);
-      } catch (error) {
+    const { id } = req.params;
+
+    if (!id) {
+      console.log(id);
+      return res.status(400).json({ error: "Missing id." });
+    }
+
+    try {
+      const cart = await Cart.findById(id);
+
+      if (!cart) {
         return res.status(400).json({ error: "Carrinho não existe." });
+      } else {
+        cart.deleteOne();
       }
+
       return res.status(200).json(cart);
     } catch (error) {
       return res.status(500).json({ error: "Internal server error." });
